745d7306b397a319f5f71f213f6b2cf8ce5db609
Noah Wager <noahwager@gmail.com>
2023-11-18 10:48:23 -0800

    Add ray class
    Signed-off-by: Noah Wager <noahwager@gmail.com>

---
+++ include/raytracer/ray.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "vec3.h"
+
+class ray {
+ public:
+  ray();
+  ray(const point3 &origin, const vec3 &direction);
+
+  point3 origin() const;
+  vec3 direction() const;
+
+  point3 at(double t) const;
+
+ private:
+  point3 orig;
+  vec3 dir;
+};
